939b0b466cee5150c83ce1d36dc12c789d4978e6
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-01-10 18:19:46 +0100

    Make universal addEventListener.

--- main.js
+++ main.js
@@ -7,19 +7,23 @@ function change() {
 }
 
 //        nasłuchiwanie i akcja
-box[0].addEventListener("click", change);
-box[1].addEventListener("click", change);
-box[2].addEventListener("click", change);
-box[3].addEventListener("click", change);
-box[4].addEventListener("click", change);
-box[5].addEventListener("click", change);
-box[6].addEventListener("click", change);
-box[7].addEventListener("click", change);
-box[8].addEventListener("click", change);
-box[9].addEventListener("click", change);
-box[10].addEventListener("click", change);
-box[11].addEventListener("click", change);
-box[12].addEventListener("click", change);
-box[13].addEventListener("click", change);
-box[14].addEventListener("click", change);
-box[15].addEventListener("click", change);
+box.forEach(element => {
+  element.addEventListener("click", change);
+});
+
+// box[0].addEventListener("click", change);
+// box[1].addEventListener("click", change);
+// box[2].addEventListener("click", change);
+// box[3].addEventListener("click", change);
+// box[4].addEventListener("click", change);
+// box[5].addEventListener("click", change);
+// box[6].addEventListener("click", change);
+// box[7].addEventListener("click", change);
+// box[8].addEventListener("click", change);
+// box[9].addEventListener("click", change);
+// box[10].addEventListener("click", change);
+// box[11].addEventListener("click", change);
+// box[12].addEventListener("click", change);
+// box[13].addEventListener("click", change);
+// box[14].addEventListener("click", change);
+// box[15].addEventListener("click", change);
